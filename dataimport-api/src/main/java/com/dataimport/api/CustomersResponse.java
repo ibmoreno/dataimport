05b3f478af43701a40da8f845d52118acb403ba7
Ivan Batista Moreno <ibmoreno.cba@gmail.com>
2024-01-08 21:49:27 -0400

    lista de clientes

---
+++ dataimport-api/src/main/java/com/dataimport/api/CustomersResponse.java
@@ -0,0 +1,48 @@
+package com.dataimport.api;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomersList {
+    private Integer id;
+    private String name;
+    private String cnpj;
+    private String address;
+    private String number;
+    private String complement;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String email;
+    private String phone;
+    private Status status;
+    private ReadModelVersion readModelVersion;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static CustomersList fromEntity(Customers customers) {
+        return CustomersList.builder()
+                .id(customers.getId())
+                .name(customers.getName())
+                .cnpj(customers.getCnpj())
+                .address(customers.getAddress())
+                .number(customers.getNumber())
+                .complement(customers.getComplement())
+                .city(customers.getCity())
+                .state(customers.getState())
+                .zipCode(customers.getZipCode())
+                .email(customers.getEmail())
+                .phone(customers.getPhone())
+                .status(customers.getStatus())
+                .readModelVersion(customers.getReadModelVersion())
+                .createdAt(customers.getCreatedAt())
+                .updatedAt(customers.getUpdatedAt()).build();
+    }
+}
---
+++ dataimport-api/src/main/java/com/dataimport/api/JacksonConfiguration.java
@@ -0,0 +1,2 @@
+package com.dataimport.api;public class JacksonConfiguration {
+}
