dd602bb807c1367525386003fc11a7fdbca4084d
Ivan Batista Moreno <ibmoreno.cba@gmail.com>
2024-02-18 00:50:34 -0400

    refatoração de código

--- dataimport-api/src/main/java/com/dataimport/api/util/KeycloakRoleConverter.java
+++ dataimport-api/src/main/java/com/dataimport/api/util/KeycloakRoleConverter.java
@@ -14,17 +14,22 @@ import static java.util.Collections.emptyMap;
 
 
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    public static final String PREFIX_RESOURCE_ROLE = "ROLE_";
+    private static final String CLAIM_RESOURCE_ACCESS = "resource_access";
+    private static final String CLAIM_ROLES = "login-client-dataimport";
+
     @Override
     public Collection<GrantedAuthority> convert(Jwt jwt) {
         final Map<String, Object> claims = jwt.getClaims();
 
         final Map<String, Map<String, List<String>>> resourceAccess =
-                (Map<String, Map<String, List<String>>>) claims.getOrDefault("resource_access", emptyMap());
+                (Map<String, Map<String, List<String>>>) claims.getOrDefault(CLAIM_RESOURCE_ACCESS, emptyMap());
 
-        Map<String, List<String>> backendRoles = resourceAccess.getOrDefault("login-client-dataimport", emptyMap());
+        Map<String, List<String>> backendRoles = resourceAccess.getOrDefault(CLAIM_ROLES, emptyMap());
 
         return backendRoles.getOrDefault("roles", emptyList()).stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
+                .map(role -> new SimpleGrantedAuthority(PREFIX_RESOURCE_ROLE + role))
                 .collect(Collectors.toList());
 
     }
