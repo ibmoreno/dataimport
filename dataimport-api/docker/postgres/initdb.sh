a64b427c2258f6bb8b475bf8d5fd8487ebcca313
Ivan Batista Moreno <ibmoreno.cba@gmail.com>
2024-02-18 23:28:10 -0400

    remove pasta docker

--- dataimport-api/docker/postgres/initdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
-  CREATE USER user_import WITH PASSWORD 'user_import';
-  CREATE DATABASE data_import OWNER user_import;
-EOSQL
